4b52861c4ba4b9779a98ceeac00f803a11e9cf2d
Jeremiah Christensen <jeremiahchris7@gmail.com>
2019-03-14 21:55:01 -0600

    create and require the router class

--- public/index.php
+++ public/index.php
@@ -6,4 +6,13 @@
  * PHP version 5.4
  */
 
-echo 'Request URL = "' . $_SERVER['QUERY_STRING'] . '"';
+// echo 'Request URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+
+$router  = new Router();
+
+echo get_class($router);
